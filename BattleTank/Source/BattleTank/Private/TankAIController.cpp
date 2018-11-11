dafe513932863e57fc67de9bbade3a39ca82f70a
x4iiiis <ryan_o_f@hotmail.co.uk>
2018-11-11 23:52:49 +0000

    AI Tanks finding the player controller
    This will soon allow them to target the player tank and shoot at it

---
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankAIController.h"
+#include "BattleTank.h"
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto PlayerTank = GetPlayerTank();
+	if (!PlayerTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AI controller can't find player tank"))
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AI found %s"), *(PlayerTank->GetName()));
+	}
+}
+
+ATank * ATankAIController::GetPlayerTank() const
+{
+	auto PlayerTank = GetWorld()->GetFirstPlayerController()->GetPawn();
+
+	if (!PlayerTank)
+	{
+		return nullptr;
+	}
+	else
+	{
+		return Cast<ATank>(PlayerTank);
+	}
+}
+
+ATank* ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+
+
